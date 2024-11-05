545b3ade6852939ce13d98a34de001a756361b21
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-11-04 21:40:41 -0800

    Added Comments

--- src/api/render_start.py
+++ src/api/render_start.py
@@ -8,7 +8,6 @@ import json
 import logging
 import sys
 
-
 description = """
 Fast-Food-Ratings shows you what others think about different food brands and their locations
 """
@@ -26,6 +25,7 @@ app = FastAPI(
 
 app.include_router(endpoints.router)
 
+
 @app.exception_handler(exceptions.RequestValidationError)
 @app.exception_handler(ValidationError)
 async def validation_exception_handler(request, exc):
@@ -38,23 +38,26 @@ async def validation_exception_handler(request, exc):
     return JSONResponse(response, status_code=422)
 
 
-"""
-root of the website
-shows opening message and all brands with their locations
-"""
+# root of the website
+# shows opening message and all brands with their locations
 @app.get("/")
 async def root():
     res = []
+
+    # gets all brands and their locations
     with db.engine.begin() as connection:
         brands = connection.execute(sqlalchemy.text("SELECT address, name FROM brands "
                                                     "JOIN locations ON b_id = brand_id"))
 
+    # iterates through each location to add them to response
     for b in brands:
         brand = {
             "brand": b.name,
             "addresses": []
         }
 
+        # checks if brand already exists in response
+        # and if it doesn't, adds it
         bIsThere = False
         for cur in res:
             if cur["brand"] == b.name:
@@ -62,9 +65,12 @@ async def root():
         if not bIsThere:
             res.append(brand)
 
+        # iterates through response to add current location/address to response
         for cur in res:
             if b.address not in cur["addresses"] and b.name == cur["brand"]:
                 cur["addresses"].append(b.address)
+
+    # adds opening message to start of response
     res.insert(0, {"message": "Welcome to Fast-Food-Ratings, for all your fast food needs."})
 
     return res
