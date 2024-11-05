12eb38f0b6a44b411f71f35f57db547c17eed78c
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-11-04 21:40:41 -0800

    Review Add create #1

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -10,6 +10,50 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
-@router.get("/{brand_id}")
-async def root():
-    return {"message": "Welcome to Fast-Food-Ratings, for all your fast food needs."}
+
+class Review(BaseModel):
+    publisher_id: int
+    description: str
+    service: int
+    quality: int
+    cleanliness: int
+
+
+# adds a review given all the known parameters
+@router.post("/{brand_id}/location/{location_id}")
+def add_review(review: Review, brand_id: int, location_id: int):
+    with db.engine.begin() as connection:
+        # gets username from user's id
+        username = connection.execute(sqlalchemy.text("SELECT name FROM users WHERE u_id = :id"),
+                                      {"id": review.publisher_id}).one().name
+
+        # gets brand name and address/location_name from ids
+        brand_dict = {"location": location_id,
+                      "brand": brand_id}
+        brand_name, address = connection.execute(
+            sqlalchemy.text("SELECT name, address FROM brands "
+                            "JOIN locations ON b_id = brand_id "
+                            "WHERE l_id = :location AND b_id = :brand"), brand_dict).one()
+
+        # adds review to reviews table
+        review_data = {
+            "l_id": location_id,
+            "p_id": review.publisher_id,
+            "description": review.description,
+            "s": review.service,
+            "q": review.quality,
+            "c": review.cleanliness
+        }
+        connection.execute(sqlalchemy.text("INSERT INTO reviews (location_id, publisher_id, description, "
+                                           "service_rating, quality_rating, cleanliness_rating) "
+                                           "VALUES (:l_id, :p_id, :description, :s, :q, :c)"), review_data)
+
+        # returns review made to user
+        res = [{
+            "publisher": username,
+            "brand": brand_name,
+            "address": address,
+            "description": review.description,
+            "ratings (S, Q, C)": [review.service, review.quality, review.cleanliness]
+        }]
+    return res
